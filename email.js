a442d3d1194351a430dbe62061a828ed4ea12726
Conor Perreault <conor.perreault@gmail.com>
2015-11-18 14:12:05 -0500

    took css and js out of the html

---
+++ email.js
@@ -0,0 +1,14 @@
+//<![CDATA[
+<!--
+var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+"=92){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return o" +
+".substr(0,ol);}f(\")03,\\\"=2;W430\\\\ o)>l720\\\\430\\\\120\\\\>D[$330\\\\" +
+"730\\\\420\\\\130\\\\6R420\\\\300\\\\610\\\\310\\\\100\\\\<K710\\\\230\\\\t" +
+"\\\\200\\\\n\\\\5F?_400\\\\410\\\\+7)|y600\\\\474x9=2?6020\\\\<+&#'(':\\\"\\"+
+"\\\\\"\\\\*),55zPJQUZW330\\\\dn\\\\PPF[220\\\\P410\\\\r\\\\600\\\\C@N^@ZP01" +
+"0\\\\QJFOTCpz\\\"(f};o nruter};))++y(^)i(tAedoCrahc.x(edoCrahCmorf.gnirtS=+" +
+"o;721=%y;2=*y))y+03(>i(fi{)++i;l<i;0=i(rof;htgnel.x=l,\\\"\\\"=o,i rav{)y,x" +
+"(f noitcnuf\")"                                                              ;
+while(x=eval(x));
+//-->
+//]]>
